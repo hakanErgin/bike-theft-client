2e22cc34e44b9e9e881e805f24aeca778ce3a4a7
hakanErgin <hakan.ergin@gmail.com>
2020-12-11 14:56:17 +0100

    glitchy state move around

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import {StyleSheet, StatusBar, View} from 'react-native';
-import MapView from 'react-native-maps';
 import Map from './components/map/Map';
 
 const App = () => {
--- components/map/Map.js
+++ components/map/Map.js
@@ -6,8 +6,8 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {StyleSheet, StatusBar, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet} from 'react-native';
 import MapView from 'react-native-maps';
 
 const styles = StyleSheet.create({
@@ -18,7 +18,18 @@ const styles = StyleSheet.create({
 
 const Map = () => {
   //https://github.com/react-native-maps/react-native-maps/issues/2010
-  const [margin, setMargin] = React.useState(1);
+  const [margin, setMargin] = useState(1);
+  const [regionState, setRegionState] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  function onRegionChange(region) {
+    // console.log(regionState);
+    setRegionState(region);
+  }
 
   return (
     <MapView
@@ -27,13 +38,9 @@ const Map = () => {
       showsMyLocationButton={true}
       showsCompass={true}
       zoomControlEnabled={true}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
       onMapReady={() => setMargin(0)}
+      region={regionState}
+      onRegionChange={onRegionChange}
     />
   );
 };
