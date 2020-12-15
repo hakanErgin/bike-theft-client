a77870137ade7e24afc51b0be42a471bbc2f4c3f
hakanErgin <hakan.ergin@gmail.com>
2020-12-15 17:15:52 +0100

    modal added

--- components/map/Map.js
+++ components/map/Map.js
@@ -1,9 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, Button, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Button, Text, View} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 import {useQuery, gql} from '@apollo/client';
+import Modal from 'react-native-modal';
 
-const bxl = {
+const BXL = {
   latitude: 50.850403778518455,
   longitude: 4.351369813084602,
   latitudeDelta: 0.0864, //y
@@ -28,17 +29,25 @@ const styles = StyleSheet.create({
   map: {
     flex: 1,
   },
+  modal: {
+    flex: 0.75,
+    backgroundColor: 'white',
+    justifyContent: 'space-between',
+    borderRadius: 20,
+    padding: 20,
+  },
 });
 
 const Map = () => {
   //https://github.com/react-native-maps/react-native-maps/issues/2010
   const [margin, setMargin] = useState(1);
-  const [regionState, setRegionState] = useState(bxl);
+  const [regionState, setRegionState] = useState(BXL);
   const [addingNewTheft, setAddingNewTheft] = useState(false);
   const [thefts, setThefts] = useState([]);
   const {loading, error} = useQuery(GET_THEFTS, {
     onCompleted: (theftsData) => setThefts(theftsData.findThefts.items),
   });
+  const [isModalVisible, setModalVisible] = useState(false);
 
   if (loading) {
     return <Text>Loading...</Text>;
@@ -46,6 +55,11 @@ const Map = () => {
   if (error) {
     console.log(error);
     return <Text>Error :(</Text>;
+  }
+
+  function hideModal() {
+    setModalVisible(false);
+    setAddingNewTheft(false);
   }
 
   function onRegionChange(region) {
@@ -58,6 +72,7 @@ const Map = () => {
       const {latitude, longitude} = theft.nativeEvent.coordinate;
       const region = {latitude, longitude};
       setThefts([...thefts, {region}]);
+      setModalVisible(true);
     }
   }
 
@@ -71,6 +86,12 @@ const Map = () => {
         title={addingNewTheft ? 'choose location' : 'add new'}
         onPress={addingNewTheftController}
       />
+      <Modal isVisible={isModalVisible}>
+        <View style={styles.modal}>
+          <Text>Hello!</Text>
+          <Button title="Hide modal" onPress={hideModal} />
+        </View>
+      </Modal>
       <MapView
         style={{...styles.map, margin}}
         showsPointsOfInterest={false}
