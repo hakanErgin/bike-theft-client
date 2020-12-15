3db1b5024d4660486110b2e89664aa323a17b560
hakanErgin <hakan.ergin@gmail.com>
2020-12-15 10:53:41 +0100

    onmappress added

--- components/map/Map.js
+++ components/map/Map.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Button} from 'react-native';
 import MapView from 'react-native-maps';
 
 const styles = StyleSheet.create({
@@ -12,28 +12,37 @@ const Map = () => {
   //https://github.com/react-native-maps/react-native-maps/issues/2010
   const [margin, setMargin] = useState(1);
   const [regionState, setRegionState] = useState({
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+    latitude: 50.850403778518455,
+    longitude: 4.351369813084602,
+    latitudeDelta: 0.0864, //y
+    longitudeDelta: 0.0535, //x
   });
+  const [addingNewTheft, setAddingNewTheft] = useState(false);
 
   function onRegionChange(region) {
-    console.log(regionState);
+    // console.log(regionState);
     setRegionState(region);
   }
 
+  function onMapPress(a) {
+    console.log(a.nativeEvent);
+  }
+
   return (
-    <MapView
-      style={{...styles.map, margin}}
-      showsUserLocation={true}
-      showsMyLocationButton={true}
-      showsCompass={true}
-      zoomControlEnabled={true}
-      onMapReady={() => setMargin(0)}
-      region={regionState}
-      onRegionChangeComplete={onRegionChange}
-    />
+    <>
+      <Button title="a" />
+      <MapView
+        style={{...styles.map, margin}}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        showsCompass={true}
+        zoomControlEnabled={true}
+        onMapReady={() => setMargin(0)}
+        region={regionState}
+        onRegionChangeComplete={onRegionChange}
+        onPress={onMapPress}
+      />
+    </>
   );
 };
 
