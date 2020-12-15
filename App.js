3ef50563337e036be211e0703ec2e5df53b49040
hakanErgin <hakan.ergin@gmail.com>
2020-12-15 14:36:40 +0100

    apolloclient, fetch, add button added

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Map from './components/map/Map';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://192.168.1.2:4000/graphql',
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Map />
-    </>
+    </ApolloProvider>
   );
 };
 
--- components/map/Map.js
+++ components/map/Map.js
@@ -1,6 +1,28 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, Button} from 'react-native';
-import MapView from 'react-native-maps';
+import {StyleSheet, Button, Text} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import {useQuery, gql} from '@apollo/client';
+
+const bxl = {
+  latitude: 50.850403778518455,
+  longitude: 4.351369813084602,
+  latitudeDelta: 0.0864, //y
+  longitudeDelta: 0.0535, //x
+};
+
+const GET_THEFTS = gql`
+  {
+    findThefts {
+      items {
+        _id
+        region {
+          latitude
+          longitude
+        }
+      }
+    }
+  }
+`;
 
 const styles = StyleSheet.create({
   map: {
@@ -11,37 +33,67 @@ const styles = StyleSheet.create({
 const Map = () => {
   //https://github.com/react-native-maps/react-native-maps/issues/2010
   const [margin, setMargin] = useState(1);
-  const [regionState, setRegionState] = useState({
-    latitude: 50.850403778518455,
-    longitude: 4.351369813084602,
-    latitudeDelta: 0.0864, //y
-    longitudeDelta: 0.0535, //x
-  });
+  const [regionState, setRegionState] = useState(bxl);
   const [addingNewTheft, setAddingNewTheft] = useState(false);
+  const [thefts, setThefts] = useState([]);
+  const {loading, error} = useQuery(GET_THEFTS, {
+    onCompleted: (theftsData) => setThefts(theftsData.findThefts.items),
+  });
+
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+  if (error) {
+    console.log(error);
+    return <Text>Error :(</Text>;
+  }
 
   function onRegionChange(region) {
     // console.log(regionState);
     setRegionState(region);
   }
 
-  function onMapPress(a) {
-    console.log(a.nativeEvent);
+  function onMapPress(theft) {
+    if (addingNewTheft === true) {
+      const {latitude, longitude} = theft.nativeEvent.coordinate;
+      const region = {latitude, longitude};
+      setThefts([...thefts, {region}]);
+    }
+  }
+
+  function addingNewTheftController() {
+    !addingNewTheft ? setAddingNewTheft(true) : setAddingNewTheft(false);
   }
 
   return (
     <>
-      <Button title="a" />
+      <Button
+        title={addingNewTheft ? 'choose location' : 'add new'}
+        onPress={addingNewTheftController}
+      />
       <MapView
         style={{...styles.map, margin}}
+        showsPointsOfInterest={false}
+        showsBuildings={false}
         showsUserLocation={true}
         showsMyLocationButton={true}
-        showsCompass={true}
         zoomControlEnabled={true}
         onMapReady={() => setMargin(0)}
         region={regionState}
         onRegionChangeComplete={onRegionChange}
-        onPress={onMapPress}
-      />
+        onPress={onMapPress}>
+        {thefts.map((theft, index) => (
+          <Marker
+            key={index}
+            coordinate={{
+              latitude: theft.region.latitude,
+              longitude: theft.region.longitude,
+            }}
+            title={'aa'}
+            description={'asdfs'}
+          />
+        ))}
+      </MapView>
     </>
   );
 };
