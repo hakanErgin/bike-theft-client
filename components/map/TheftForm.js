ac741966711ba258399b2a2454f8e2be4dc27578
hakanErgin <hakan.ergin@gmail.com>
2020-12-16 14:16:16 +0100

    form (pkg) added

--- components/map/TheftForm.js
+++ components/map/TheftForm.js
@@ -1,8 +1,49 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, TextInput, Button, StyleSheet} from 'react-native';
+import {useForm, Controller} from 'react-hook-form';
 
-const TheftForm = () => {
-  return <Text>Hello!</Text>;
-};
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+  },
+});
 
-export default TheftForm;
+export default function TheftForm() {
+  const {control, handleSubmit, errors} = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <View>
+      <Controller
+        control={control}
+        render={({onChange, onBlur, value}) => (
+          <TextInput
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={(val) => onChange(val)}
+            value={value}
+          />
+        )}
+        name="description"
+        rules={{required: true}}
+        defaultValue="please describe your bike here"
+      />
+      {errors.description && <Text>This is required.</Text>}
+
+      <Controller
+        control={control}
+        render={({onChange, onBlur, value}) => (
+          <TextInput
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={(val) => onChange(val)}
+            value={value}
+          />
+        )}
+        name="comments"
+        defaultValue="comments here"
+      />
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+    </View>
+  );
+}
