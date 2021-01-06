8a5ffa628f1b5efb57fff4a79758e437b3c1f881
hakanErgin <hakan.ergin@gmail.com>
2021-01-06 15:25:40 +0100

    drawer added, wip

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import MapScreen from './screens/MapScreen';
 import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import CustomDrawerContent from './components/CustomDrawerContent';
+
+const Drawer = createDrawerNavigator();
 
 const client = new ApolloClient({
   uri: 'http://192.168.1.2:4000/graphql',
@@ -10,7 +21,20 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <MapScreen />
+      <NavigationContainer>
+        <Drawer.Navigator
+          initialRouteName="Map"
+          drawerContent={(props) => {
+            return (
+              <DrawerContentScrollView {...props}>
+                {/* <DrawerItemList {...props} /> */}
+                <CustomDrawerContent />
+              </DrawerContentScrollView>
+            );
+          }}>
+          <Drawer.Screen name="Map" component={MapScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
     </ApolloProvider>
   );
 };
---
+++ components/CustomDrawerContent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const CustomDrawerContent = () => {
+  return <Text>asd</Text>;
+};
+
+export default CustomDrawerContent;
