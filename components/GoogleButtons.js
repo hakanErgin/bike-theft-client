6a4983f77048ddf501ebc6356830f1ddaa9f39b5
hakanErgin <hakan.ergin@gmail.com>
2021-01-08 02:58:51 +0100

    gBtns toggle isuserloggedin

--- components/GoogleButtons.js
+++ components/GoogleButtons.js
@@ -5,12 +5,17 @@ import {
   GoogleSigninButton,
 } from '@react-native-community/google-signin';
 import {Text, View, Button} from 'react-native';
-
 import {WEB_CLIENT_ID} from '@env';
 import {CREATE_USER_OR_SIGN_IN} from '../shared/gql';
 import {useMutation} from '@apollo/client';
+import {
+  useIsUserLoggedIn,
+  useToggleIsUserLoggedIn,
+} from '../shared/IsUserLoggedInContext';
 
 export const SignInButton = () => {
+  const setIsUserLoggedIn = useToggleIsUserLoggedIn();
+
   GoogleSignin.configure({
     webClientId: WEB_CLIENT_ID,
     offlineAccess: true,
@@ -26,7 +31,10 @@ export const SignInButton = () => {
       const userInfo = await GoogleSignin.signIn();
       createUserOrSignInMutation({
         variables: {id_token: userInfo.idToken},
-      }).then((result) => console.log(result));
+      }).then((result) => {
+        setIsUserLoggedIn(true);
+        console.log({result});
+      });
     } catch (error) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
         console.log({error});
@@ -53,23 +61,28 @@ export const SignInButton = () => {
 };
 
 export const LogoutButton = () => {
+  const setIsUserLoggedIn = useToggleIsUserLoggedIn();
+
   const signOut = async () => {
     try {
       await GoogleSignin.revokeAccess();
-      await GoogleSignin.signOut();
+      await GoogleSignin.signOut().then(() => setIsUserLoggedIn(false));
     } catch (error) {
-      console.error(error);
+      console.log(error);
     }
   };
   return <Button title={'logout'} onPress={signOut} />;
 };
 
 export const CheckUserButton = () => {
+  const isUserLoggedIn = useIsUserLoggedIn();
+
   async function checkUser() {
     const isSignedIn = await GoogleSignin.isSignedIn();
     console.log({isSignedIn});
     const currentUser = await GoogleSignin.getCurrentUser();
     console.log({currentUser});
+    console.log({isUserLoggedIn});
   }
   return <Button title={'status'} onPress={checkUser} />;
 };
