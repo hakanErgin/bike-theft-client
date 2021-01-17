9955b29e688f7ec39bd1a2e0e21f9b0c29875450
hakanErgin <hakan.ergin@gmail.com>
2021-01-17 16:52:06 +0100

    gettheft query

--- src/Views/ModalContent/ViewModal.js
+++ src/Views/ModalContent/ViewModal.js
@@ -1,28 +1,32 @@
-import React, {useState} from 'react';
-import {Button, Text, View, TextInput} from 'react-native';
+import React from 'react';
+import {Button, Text, View} from 'react-native';
 import styles from './modalStyles';
 import Modal from 'react-native-modal';
-import {useMutation} from '@apollo/client';
-import {GET_THEFTS, DELETE_THEFT} from '../../Utils/gql';
+import {useQuery, useMutation} from '@apollo/client';
+import {GET_THEFTS, DELETE_THEFT, GET_THEFT} from '../../Utils/gql';
 
 const ViewModal = ({
   isViewModalVisible,
   setIsViewModalVisible,
   selectedTheftId,
 }) => {
-  //#region del mutate
+  //#region
+  const {error: get_error, data: get_data} = useQuery(GET_THEFT, {
+    variables: {id: selectedTheftId},
+  });
+
   const [
     submitDeleteMutation,
     {error: delete_error},
   ] = useMutation(DELETE_THEFT, {refetchQueries: [{query: GET_THEFTS}]});
 
-  const deleteTheft = (theftId) => () => {
+  const deleteTheft = () => {
     submitDeleteMutation({
-      variables: {input: {_id: theftId}},
+      variables: {input: {_id: selectedTheftId}},
     });
   };
 
-  if (delete_error) {
+  if (delete_error || get_error) {
     console.log(delete_error);
   }
 
@@ -30,20 +34,22 @@ const ViewModal = ({
 
   //#endregion
 
-  return (
+  return get_data ? (
     <Modal isVisible={isViewModalVisible}>
       <View style={styles.modal}>
         <Text>yes</Text>
-        <Text>{selectedTheftId}</Text>
+        <Text>{get_data.getTheft.region.latitude}</Text>
+        <Text>{get_data.getTheft.region.longitude}</Text>
         <Button
           title={'close'}
           onPress={() => {
             setIsViewModalVisible(false);
           }}
         />
+        <Button title={'delete'} onPress={deleteTheft} />
       </View>
     </Modal>
-  );
+  ) : null;
 };
 
 export default ViewModal;
