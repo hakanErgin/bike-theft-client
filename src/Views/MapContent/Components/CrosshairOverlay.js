b79bb8034933000323126852b9bb9102fb551dc8
hakanErgin <hakan.ergin@gmail.com>
2021-01-29 00:58:20 +0100

    styles: menu refactor

--- src/Views/MapContent/Components/CrosshairOverlay.js
+++ src/Views/MapContent/Components/CrosshairOverlay.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import {Text} from 'react-native';
-import styles from '../mapStyles';
+import {StyleSheet, Text} from 'react-native';
 
 export default function CrosshairOverlay() {
   return <Text style={styles.crosshair}>+</Text>;
 }
+
+const styles = StyleSheet.create({
+  crosshair: {
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    fontSize: 50,
+    color: 'white',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
+});
