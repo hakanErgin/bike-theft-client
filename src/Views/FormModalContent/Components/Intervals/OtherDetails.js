3e9abb86004e571c7e72b1e197d79c1b5b5463ef
hakanErgin <hakan.ergin@gmail.com>
2021-03-25 13:57:21 +0100

    check box added

--- src/Views/FormModalContent/Components/Intervals/OtherDetails.js
+++ src/Views/FormModalContent/Components/Intervals/OtherDetails.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, TextInput, StyleSheet} from 'react-native';
 import commonStyles from '../../../../Utils/commonStyles';
 import {NormalText} from '../../../../Utils/commonComponents';
+import CheckBox from '@react-native-community/checkbox';
 
 export const OtherDetails = ({handleChange, values}) => {
+  const [isSharingContact, setIsSharingContact] = useState(false);
   return (
     <View style={styles.slide}>
       <NormalText>Anything else you like to add..</NormalText>
@@ -15,6 +17,22 @@ export const OtherDetails = ({handleChange, values}) => {
         numberOfLines={8}
         placeholder="Comments"
       />
+      <View style={styles.contactInformation}>
+        <CheckBox
+          tintColors={{
+            true: commonStyles.iconColor.darkRed,
+            false: commonStyles.iconColor.lightGrey,
+          }}
+          value={isSharingContact}
+          onValueChange={(value) => {
+            setIsSharingContact(value);
+          }}
+        />
+
+        <NormalText style={styles.ebikeText}>
+          I want to share my contact information
+        </NormalText>
+      </View>
     </View>
   );
 };
@@ -31,6 +49,7 @@ const styles = StyleSheet.create({
     marginBottom: commonStyles.gap[3],
     width: '100%',
   },
+  contactInformation: {flexDirection: 'row', alignItems: 'center'},
   slide: {
     paddingHorizontal: commonStyles.gap[5],
     paddingBottom: commonStyles.gap[3],
